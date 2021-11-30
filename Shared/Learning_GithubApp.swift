13c5563d8d95da244372e2fbfd9d9416d1d16caa
Axel Whalen <axel.whalen@pangea.art>
2021-11-30 09:56:08 -0800

    Initial Commit

---
+++ Shared/Learning_GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GithubApp.swift
+//  Shared
+//
+//  Created by Axel Whalen on 2021-11-30.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
